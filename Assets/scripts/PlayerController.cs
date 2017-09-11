fdcd2cddcdef26d1b84ef5bbc4d07e0b5d60a437
mfrisell <mfrisell@kth.se>
2017-09-11 12:09:53 +0200

    initialize

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+	private GameObject newProjectile;
+
+	public float fireDelta = 0.5F;
+	private float nextFire = 0.5F;
+	private float myTime = 0.0F;
+
+	void Update() {
+
+		myTime = myTime + Time.deltaTime;
+
+		if (Input.GetButton("Fire1") && myTime > nextFire)
+		{
+			nextFire = myTime + fireDelta;
+
+			StartCoroutine(Shoot());
+
+
+			// create code here that animates the newProjectile
+
+			nextFire = nextFire - myTime;
+			myTime = 0.0F;
+		}
+
+	}
+
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+	
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		Rigidbody rb = GetComponent<Rigidbody> ();
+		rb.velocity = movement * speed;
+
+	}
+
+	IEnumerator Shoot() {
+
+		AudioSource audio = GetComponent<AudioSource> ();
+		audio.Play ();
+		yield return new WaitForSeconds (0.5f);
+		newProjectile = Instantiate(shot, shotSpawn.position, shotSpawn.rotation) as GameObject;
+	}
+
+}
---
+++ Assets/scripts/RandomWobble.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomWobble : MonoBehaviour {
+
+
+	public float fireDelta = 0.5F;
+	private float nextFire = 0.5F;
+	private float myTime = 0.0F;
+
+
+	public float ScalingFactor = 1f;
+	private float TimeScale = 2f;
+	private float TimeScaleDown = 10f;
+	private Vector3 InitialScale;
+	private Vector3 FinalScale;
+
+
+	void Start () {
+
+		InitialScale = transform.localScale;
+		FinalScale = new Vector3(InitialScale.x + ScalingFactor, 
+		InitialScale.y + ScalingFactor/2,
+		InitialScale.z + ScalingFactor);
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		myTime = myTime + Time.deltaTime;
+
+		if (Input.GetButton("Fire1") && myTime > nextFire)
+		{
+			nextFire = myTime + fireDelta;
+
+			StartCoroutine (LerpUp ());
+
+			nextFire = nextFire - myTime;
+			myTime = 0.0F;
+		}
+			
+	}
+
+	IEnumerator LerpUp(){
+		float progress = 0;
+
+
+		while(progress <= 1){
+			transform.localScale = Vector3.Slerp(InitialScale, FinalScale,  Mathf.SmoothStep(0.0f, 1.0f, progress));
+			progress += Time.deltaTime * TimeScale;
+			yield return null;
+		}
+		transform.localScale = FinalScale;
+		StartCoroutine (LerpDown ());
+
+	} 
+
+	IEnumerator LerpDown(){
+		float progress = 0;
+
+		while(progress <= 1){
+			transform.localScale = Vector3.Slerp(FinalScale, InitialScale,  Mathf.SmoothStep(0.0f, 1.0f, progress));
+			progress += Time.deltaTime * TimeScaleDown;
+			yield return null;
+		}
+		transform.localScale = InitialScale;
+
+
+	} 
+
+
+}
