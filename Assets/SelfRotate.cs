2fd271f9dd0053627f4e39013ecb47f4c3e20c95
Jonathan Golan <Jonathanbgolan@gmail.com>
2017-09-14 16:08:40 +0200

    Asteroid changes
    Prefab etc

--- Assets/SelfRotate.cs
+++ Assets/SelfRotate.cs
@@ -2,13 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Die : MonoBehaviour {
+public class SelfRotate : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
-        Destroy(this.gameObject, 7f);
-    }
+		
+	}
 	
 	// Update is called once per frame
 	void Update () {
