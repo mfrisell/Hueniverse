0a062fbfdcd349f7d5cd38b01e257e12a1caab1a
mfrisell <mfrisell@kth.se>
2017-09-20 17:03:27 +0200

    Score and fixed (update game controller) timer working!

---
+++ Assets/ShowProgress.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowProgress : MonoBehaviour {
+
+	public float gameProgress;
+	private float timer;
+
+	// Use this for initialization
+	void Start () {
+
+		GameObject gameControllerObject = GameObject.FindGameObjectWithTag ("GameController");
+		GameController gameControllerScript = gameControllerObject.GetComponent<GameController> ();
+
+		// Change when maxGameTime is added in GameController
+		//gameProgress = gameControllerScript.maxGameTime;
+
+		gameProgress = 180;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Get time, convert to range (0,1)
+		timer += Time.deltaTime;
+		float timeToProgress = map01 (timer, 0, gameProgress);
+
+		Image progress = GetComponent<Image>();
+		progress.fillAmount = timeToProgress;
+	}
+
+	public static float map01( float value, float min, float max )  
+	{
+		return ( value - min ) * 1f / ( max - min );
+	}
+}
