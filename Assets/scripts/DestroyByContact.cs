daba60be215165277725d42580cb03f58d240b7f
mfrisell <mfrisell@kth.se>
2017-09-21 15:11:40 +0200

    Explosions fixed for all colors

--- Assets/scripts/DestroyByContact.cs
+++ Assets/scripts/DestroyByContact.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour {
 
+	public GameObject explosion;
+
 	void OnTriggerEnter(Collider other) {
-//		Destroy (other.gameObject);
-//		Destroy (gameObject);
 
 		if (gameObject.tag == other.tag) {
-			Destroy (gameObject);
+			// Create Explosion
+			Instantiate (explosion, transform.position, transform.rotation);
+
+			// Update player score
 			GameObject go = GameObject.FindGameObjectWithTag ("GameController");
 			GameController gco = go.GetComponent<GameController> ();
 			gco.score += 10;
+
+			// Hide mesh
+			MeshRenderer mesh = GetComponent<MeshRenderer> ();
+			mesh.enabled = false;
+
+			// Fade out audio
+			StartCoroutine (fadeAudio ());
 		}
+	}
+
+	IEnumerator fadeAudio() {
+		
+		for (int i = 0; i < 10; i++) {
+			AudioSource audio = GetComponent<AudioSource> ();
+			Debug.Log (audio.volume);
+			audio.volume -= 0.02f;
+			yield return new WaitForSeconds (0.05f);
+		}
+
+		// Remove gameobject from scene
+		Destroy (gameObject);
 	}
 }
