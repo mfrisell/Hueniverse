ba11f599f8090c466f9202d759e8ab2366a1ec7b
mfrisell <mfrisell@kth.se>
2017-09-24 22:21:40 +0200

    scale up colored planets on instantiate

---
+++ Assets/ScaleUp.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleUp : MonoBehaviour
+{
+	public float scaleTime = 0.1f;
+
+	// Use this for initialization
+	void Start ()
+	{
+
+		StartCoroutine (Scale ());
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+
+	IEnumerator Scale ()
+	{
+
+		for (int i = 0; i <= 100; i+=5) {
+
+			transform.localScale = new Vector3 (i, i, i);
+			yield return new WaitForSeconds (scaleTime * Time.deltaTime);
+
+		}
+	}
+}
