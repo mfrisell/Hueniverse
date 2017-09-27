e54d6d78fde5b410b1ddbd08f6af38a5a458af9a
mfrisell <mfrisell@kth.se>
2017-09-27 14:35:22 +0200

    startscene finished hopefully

---
+++ Assets/scripts/RunScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunScene : MonoBehaviour {
+	private string scene;
+
+	void OnTriggerEnter(Collider other) {
+
+		if (gameObject.tag == "start") {
+			scene = "main";
+		}
+		else if(gameObject.tag == "tutorial") {
+			scene = "Tutorial";
+		}
+
+		StartCoroutine (changeScene (scene));
+	}
+
+	// Run chosen scene after 3 seconds
+	IEnumerator changeScene(string scene) {
+		yield return new WaitForSeconds (3);
+
+		// Run scene
+		Application.LoadLevel(scene);
+	}
+}
