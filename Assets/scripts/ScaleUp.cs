1efc60eff764b425911a58e9ad189777aba57cfc
mfrisell <mfrisell@kth.se>
2017-09-27 20:33:41 +0200

    GameOver 3d Model added

--- Assets/scripts/ScaleUp.cs
+++ Assets/scripts/ScaleUp.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ScaleUp : MonoBehaviour
 {
 	public float scaleTime = 0.1f;
+	public float scaleFactor = 1f;
 
 	// Use this for initialization
 	void Start ()
@@ -26,7 +27,7 @@ public class ScaleUp : MonoBehaviour
 
 		for (int i = 0; i <= 100; i+=5) {
 
-			transform.localScale = new Vector3 (i, i, i);
+			transform.localScale = new Vector3 (i * scaleFactor, i * scaleFactor, i * scaleFactor);
 			yield return new WaitForSeconds (scaleTime * Time.deltaTime);
 
 		}
