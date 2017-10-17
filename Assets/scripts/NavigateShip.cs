2c7dfcc7b6c289d6837972856392b48e2cd2afa6
Rasmus Fredrikson <rasmuf@kth.se>
2017-10-17 12:33:39 +0200

    ship moves when the player tilts their head

--- Assets/scripts/NavigateShip.cs
+++ Assets/scripts/NavigateShip.cs
@@ -1,19 +1,26 @@
-﻿using System;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
 
 
-public class NavigateShip
-{
-	private GameObject camera;
-	private GameObject spaceShip;
-
-	void Start() 
-	{
-		
-	}
+public class NavigateShip : MonoBehaviour {
+	public GameObject cameraHead;
+	public GameObject spaceShip;
 
 	void Update()
 	{
-		
+		spaceShip.transform.rotation = cameraHead.transform.rotation;
+		if (cameraHead.transform.rotation.z < -10 || Input.GetKeyDown ("a")) {
+			Debug.Log ("Pressing a");
+			if(spaceShip.transform.position.x > -8)
+				spaceShip.transform.position = new Vector3 (spaceShip.transform.position.x - 1, -0.3, 0.5);
+		}
+		if (cameraHead.transform.rotation.z > 10 || Input.GetKeyDown ("d")) {
+			Debug.Log ("Pressing d");
+			if(spaceShip.transform.position.x < 8)
+				spaceShip.transform.position = new Vector3 (spaceShip.transform.position.x + 1, -0.3, 0.5);
+		}
 	}
 }
 
